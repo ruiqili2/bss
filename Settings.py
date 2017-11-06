4c4618951f14b9116a5e13c2fab901816f815bfd
Ruiqi Li <ruiqili2@illinois.edu>
2017-11-06 01:44:39 -0600

    api not working

--- Settings.py
+++ Settings.py
@@ -8,4 +8,6 @@ DIRNAME = os.path.dirname(__file__)
 STATIC_PATH = os.path.join(DIRNAME, 'static')
 TEMPLATE_PATH = os.path.join(DIRNAME, 'templates')
 #WORKDIR = os.path.join(STATIC_PATH, "workdir/")
-
+with open('config/ranC.tk', 'r') as fi:
+    COOKIE_SECRET = fi.readline().strip()
+    SKEY = fi.readline().strip()
--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import tornado.web
 import MySQLdb as mydb
 import os.path
 import Settings
+import api
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('index.html')
+class My404Handler(tornado.web.RequestHandler):
+    def prepare(self):
+        self.set_status(404)
+        self.render('404.html', errormessage='404: Page Not Found', username="")
 
 class Application(tornado.web.Application):
     """
@@ -16,11 +21,15 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", MainHandler),
+            (r"/bss/create", api.CreateHandler),
         ]
         settings = {
             "template_path": Settings.TEMPLATE_PATH,
             "static_path": Settings.STATIC_PATH,
             "debug": Settings.DEBUG,
+            "cookie_secret": Settings.COOKIE_SECRET,
+            "static_url_prefix": "/static/",
+            "default_handler_class": "My404Handler",
         }
         tornado.web.Application.__init__(self, handlers, **settings)
 
